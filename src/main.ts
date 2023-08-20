76163a05cc3088795ea4894242da686324cff629
Raphael Forment <raphael.forment@gmail.com>
2023-08-20 23:12:15 +0200

    some things better some things worse

--- src/main.ts
+++ src/main.ts
@@ -31,17 +31,18 @@ const classMap = {
   h1: "text-white lg:text-4xl text-xl lg:ml-4 lg:mx-4 mx-2 lg:my-4 my-2 lg:mb-8 mb-4 bg-neutral-900 rounded-lg py-2 px-2",
   h2: "text-white lg:text-3xl text-xl lg:ml-4 lg:mx-4 mx-2 lg:my-4 my-2 lg:mb-8 mb-4 bg-neutral-900 rounded-lg py-2 px-2",
   ul: "text-underline",
-  li: "ml-12 list-disc lg:text-2xl text-base text-white mx-4 my-4 leading-normal",
+  li: "ml-12 list-disc lg:text-2xl text-base text-white lg:mx-4 mx-2 my-4 lg:pl-4 my-2 leading-normal",
   p: "lg:text-2xl text-base text-white lg:mx-4 mx-2 my-4 leading-normal",
   a: "lg:text-2xl text-base text-orange-300",
-  code: "my-4 text-base lg:text-xl block whitespace-pre overflow-x-scroll",
-  icode: "my-4 text-white font-mono bg-neutral-600",
+  code: "lg:my-4 sm:my-1 text-base lg:text-xl block whitespace-pre overflow-x-scroll",
+  icode:
+    "lg:my-4 my-1 lg:text-xl sm:text-xs text-white font-mono bg-neutral-600",
   blockquote: "text-neutral-200 border-l-4 border-neutral-500 pl-4 my-4 mx-4",
   table:
-    "justify-center my-8 mx-8 text-2xl w-full text-left text-white border-collapse",
+    "justify-center lg:my-8 my-2 lg:mx-8 mx-2 lg:text-2xl text-base w-full text-left text-white border-collapse",
   thead:
     "text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400",
-  th: "px-6 py-6",
+  th: "lg:px-6 lg:py-6 px-2 py-2",
   td: "py-2",
   tr: "py-0",
 };
