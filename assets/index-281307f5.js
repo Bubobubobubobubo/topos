218032e2feef41a3c82033a9433985fb616f0602
Bubobubobubobubo <Bubobubobubobubo@users.noreply.github.com>
2023-08-28 12:20:27 +0000

    deploy: 30c795430f80a26d86b1550bf46a52ed0ce15a51

--- assets/index-281307f5.js
+++ assets/index-281307f5.js
@@ -866,6 +866,12 @@ ${t("Demonstrative filtering. Final list is [100, 200]",`
 mod(1)::snd('sine').sustain(0.1).freq([100,100,100,100,200].unique().beat()).out()
 `,!0)}
 
+- <icode>add()</icode>: add a given amount to every list element.
+- <icode>sub()</icode>: add a given amount to every list element.
+- <icode>mult()</icode>: add a given amount to every list element.
+- <icode>division()</icode>: add a given amount to every list element. The method is named <icode>division</icode> because obviously <icode>div</icode> is already taken.
+
+${t("Simple addition","[1, 2 ,3].add(2).beat()",!0)}
 
 ## Simple patterns 
 	
@@ -994,18 +1000,26 @@ The code you enter in any of the scripts is evaluated in strict mode. This tells
 
 There are some techniques that Topos players are using to keep their JavaScript short and tidy. Don't try to write the shortest possible code but use shortcuts when it makes sense. It's sometimes very comforting to take time to write utilities and scripts that you will often reuse. Take a look at the following examples:
 
-${t("Shortening your if conditions",`// The && symbol (overriden by :: in Topos) is very often used for conditions!
-mod(.75) :: snd('zap').out()
+${t("Shortening your if conditions",`
+// The && symbol (overriden by :: in Topos) is very often used for conditions!
+mod(.75) :: snd('linnhats').n([1,4,5].beat()).out()
+mod(1) :: snd('bd').out()
 //if (true) && log('very true')
+// These two lines are the same:
+// mod(1) && snd('bd').out() 
+//// mod(1) :: snd('bd').out() 
+
 `,!0)}
 
-${t("More complex conditions using ?",`// The ? symbol can be used to write a if/true/false condition
+${t("More complex conditions using ?",`
+// The ? symbol can be used to write a if/true/false condition
 mod(4) ? snd('kick').out() : mod(2)::snd('snare').out()
 // (true) ? log('very true') : log('very false')
 `,!1)}
 
 
-${t("Using not and other short symbols",`// The ! symbol can be used to reverse a condition
+${t("Using not and other short symbols",`
+// The ! symbol can be used to reverse a condition
 mod(4) ? snd('kick').out() : mod(2)::snd('snare').out()
 !mod(2) :: mod(0.5)::snd('clap').out()
 `,!1)}
