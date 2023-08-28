a00c22851f703c9ffdacea34cdcf00ab948921cb
Raphael Forment <raphael.forment@gmail.com>
2023-08-28 17:45:22 +0200

    adding new landing page song

--- src/examples/excerpts.ts
+++ src/examples/excerpts.ts
@@ -1,5 +1,22 @@
 export const examples = [
 `
+// Ancient rhythms - Bubobubobubo
+mod(1)::snd('kick').out();
+mod(2)::snd('sd').room(0.9).size(0.9).out();
+mod(0.25)::snd('hh').out();
+mod(2)::snd('square')
+  .cutoff(500).note(50-12).resonance(20).sustain(0.2).out()
+mod(1/4)::snd(divseq(1, 'sawtooth', 'triangle', 'pulse'))
+  .note(divseq(4, 50, 53, 55, 50, 50, 52, 58, 50+12, 50+15) + divseq(0.5, 0, 12, 24))
+  .cutoff(usine(.5)*10000).resonance(divseq(2, 10,20))
+  .fmi($(1) % 10).fmh($(2) % 5)
+  .room(0.8).size(0.9)
+  .delay(0.5).delaytime(0.25)
+  .delayfb(0.6)
+  .sustain(0.01 + usine(.25) / 10).out()
+mod(4)::snd('amencutup').n($(19)).cut(1).orbit(2).pan(rand(0.0,1.0)).out()
+log(bar(), beat(), pulse())`,
+`
 // Crazy arpeggios - Bubobubobubo
 bpm(110)
 mod(0.125) && sound('sawtooth')
