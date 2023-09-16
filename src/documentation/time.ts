4eea3896ba0c28a853028422252abf53a432ef0b
Raphael Forment <raphael.forment@gmail.com>
2023-09-17 01:12:05 +0200

    fix last broken example

--- src/documentation/time.ts
+++ src/documentation/time.ts
@@ -108,7 +108,7 @@ ${makeExample(
   bpm(200)
   oncount([1, 5, 9, 13],16) :: sound('bd').gain(1.0).out()
   oncount([5, 6, 13],16) :: sound('cp').gain(0.9).out()
-  oncount([2, 3, 3. 5, 6, 7, 10, 15],16) :: sound('hh').n(8).gain(0.8).out()
+  oncount([2, 3, 3.5, 6, 7, 10, 15],16) :: sound('hh').n(8).gain(0.8).out()
   oncount([1, 4, 5, 8, 9, 10, 11, 12, 13, 14, 15, 16],16) :: 
   sound('hh').out()
 `,
