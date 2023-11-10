78eb181935993051637f644911c424b890de2427
Raphael Forment <raphael.forment@gmail.com>
2023-11-10 10:14:13 +0100

    adding more for pwa

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,39 @@
 import { defineConfig } from "vite";
 import { VitePWA } from 'vite-plugin-pwa';
-
 // import * as mdPlugin from 'vite-plugin-markdown';
+
+const webManifest = {
+  name: "Topos",
+  short_name: "Topos",
+  description: "Live coding environment",
+  theme_color: "#ffffff",
+  icons: [
+    {
+      src: 'favicon/android-chrome-192x192.png',
+      sizes: '192x192',
+      type: 'image/png'
+    },
+    {
+      src: 'favicon/android-chrome-512x512.png',
+      sizes: '512x512',
+      type: 'image/png'
+    },
+  ]
+}
 
 export default defineConfig(({ command, mode, ssrBuild }) => {
   if (command === "serve") {
     return {
       plugins: [
-        VitePWA({ registerType: 'autoUpdate' })
+        VitePWA({
+          includeAssets: [
+            'favicon/favicon.icon',
+            'favicon/apple-touch-icon.png',
+            'mask-icon.svg'
+          ],
+          manifest: webManifest,
+          registerType: 'autoUpdate'
+        })
       ],
       assetsInclude: ["**/*.md"],
       server: {
@@ -18,7 +44,15 @@ export default defineConfig(({ command, mode, ssrBuild }) => {
   } else {
     return {
       plugins: [
-        VitePWA({ registerType: 'autoUpdate' })
+        VitePWA({
+          includeAssets: [
+            'favicon/favicon.icon',
+            'favicon/apple-touch-icon.png',
+            'mask-icon.svg'
+          ],
+          manifest: webManifest,
+          registerType: 'autoUpdate'
+        })
       ],
       chunkSizeWarningLimit: 1600 * 2,
       build: {
