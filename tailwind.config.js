ce1f005f07b6bd012a34bb4d18e981402309a853
Raphael Forment <raphael.forment@gmail.com>
2023-12-15 13:39:48 +0100

    add tailwind css variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,18 @@ export default {
     },
   ],
   theme: {
+    colors: {
+      background: "var(--background)",
+      lineBackground: "var(--lineBackground)",
+      foreground: "var(--foreground)",
+      caret: "var(--caret)",
+      selection: "var(--selection)",
+      selectionMatch: "var(--selectionMatch)",
+      gutterBackground: "var(--gutterBackground)",
+      gutterForeground: "var(--gutterForeground)",
+      gutterBorder: "var(--gutterBorder)",
+      lineHighlight : "var(--lineHighlight)",
+    },
     extend: {},
     hljs: {
       theme: "nord",
