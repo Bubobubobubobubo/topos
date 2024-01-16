588934d113fc3a3373679a803f66a91556b91fe1
Raphael Forment <raphael.forment@gmail.com>
2024-01-16 23:49:33 +0100

    adding a convenience drumMachine function

--- src/classes/SoundEvent.ts
+++ src/classes/SoundEvent.ts
@@ -41,6 +41,19 @@ export class SoundEvent extends AudibleEvent {
     zrand: ["zrand", "zr"],
     curve: ["curve"],
     bank: ["bank"],
+    drumMachine: function(self: SoundEvent, a: number) {
+      let machines = ["AJKPercusyn", "AkaiLinn", "AkaiMPC60", "AkaiXR10", "AlesisHR16", "AlesisSR16", "BossDR110", "BossDR220", "BossDR55",
+        "BossDR550", "BossDR660", "CasioRZ1", "CasioSK1", "CasioVL1", "DoepferMS404", "EmuDrumulator", "EmuModular", "EmuSP12",
+        "KorgDDM110", "KorgKPR77", "KorgKR55", "KorgKRZ", "KorgM1", "KorgMinipops", "KorgPoly800", "KorgT3", "Linn9000",
+        "LinnDrum", "LinnLM1", "LinnLM2", "MFB512", "MPC1000", "MoogConcertMateMG1", "OberheimDMX", "RhodesPolaris",
+        "RhythmAce", "RolandCompurhythm1000", "RolandCompurhythm78", "RolandCompurhythm8000", "RolandD110", "RolandD70", "RolandDDR30",
+        "RolandJD990", "RolandMC202", "RolandMC303", "RolandMT32", "RolandR8", "RolandS50", "RolandSH09", "RolandSystem100", "RolandTR505",
+        "RolandTR606", "RolandTR626", "RolandTR707", "RolandTR727", "RolandTR808", "RolandTR909", "SakataDPM48", "SequentialCircuitsDrumtracks",
+        "SequentialCircuitsTom", "SergeModular", "SimmonsSDS400", "SimmonsSDS5", "SoundmastersR88", "UnivoxMicroRhythmer12", "ViscoSpaceDrum",
+        "XdrumLM8953", "YamahaRM50", "YamahaRX21", "YamahaRX5", "YamahaRY30", "YamahaTG33"];
+      self.updateValue("bank", machines[a % machines.length]);
+      return self;
+    },
     slide: ["slide", "sld"],
     deltaSlide: ["deltaSlide", "dslide"],
     pitchJump: ["pitchJump", "pj"],
