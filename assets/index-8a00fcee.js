c590ce6ecb156c150ca410a898ed3eea43d8ee9a
Bubobubobubobubo <Bubobubobubobubo@users.noreply.github.com>
2024-03-05 15:27:39 +0000

    deploy: e592499711959f08cf896af9e08f3c688854d552

--- assets/index-8a00fcee.js
+++ assets/index-8a00fcee.js
@@ -314,6 +314,20 @@ You can also find a .pdf version listing the principal commands and functions [h
 `},FB=r=>{const e=St(r);return`
 # Atelier (06 mars 2024)
 
+Bonjour tout le monde ! Nous sommes :
+
+- [Rémi Georges](https://remigeorges.fr) : musicien, réalisateur en informatique musicale.
+- [Agathe Herrou](https://www.youtube.com/@th4music) : musicienne, chercheuse.
+- [Raphaël Forment](https://raphaelforment.fr) : musicien, doctorant.
+
+Nous pratiquons le [live coding](https://livecoding.fr). Nous utilisons notre ordinateur comme un instrument de musique, nous programmons de la musique devant notre public. Nous pouvons faire plein de choses comme :
+
+- créer des instruments de musique, des synthétiseurs, des boîtes à rythme.
+- jouer des échantillons, charger des images, des vidéos, créer des animations.
+- contrôler d'autres instruments, jouer avec d'autres musiciens.
+
+Topos est un instrument de musique. On peut l'utiliser depuis n'importe quel ordinateur, sans avoir à installer quoi que ce soit. Nous l'avons fabriqué pour que tout le monde puisse jouer facilement de la musique.
+
 
 ## Découverte
 
@@ -325,6 +339,8 @@ beat(1)::sound('kick').out()
 beat(2)::sound('snare').out()
 beat(.5)::sound('hh').out()
 `,!0)}
+
+<br>
 
 - Qu'est-ce qu'il se passe si je change un nombre ?
 - Qu'est-ce qu'il se passe si je change un nom ? 
@@ -347,6 +363,8 @@ beat(.5)::sound('hh').out()
 beat([0.25,0.5].beat(1))::sound("pluck")
   .note([40,45].beat(2)).out()
 `,!0)}
+
+<br>
 
 - Qu'est-ce que le son <ic>"pluck"</ic> ?
 
@@ -376,11 +394,15 @@ beat([0.25,0.5].beat())::sound("pluck")
   .out()
 `,!0)}
 
+<br>
+
 Ici, on ajoute une nouvelle mélodie mais il s'agit aussi d'un nouvel instrument. C'est pour cela que le code est plus long. Quand on fait du <em>live coding</em>, on code tout en même temps : notes, rythmes, mélodies, sons. C'est beaucoup de choses ! C'est pour cela que le code est court, on essaie de tout taper très vite en jouant !
 
 - Que signifie selon vous <ic>vib</ic>, <ic>delay</ic>, <ic>room</ic> ou <ic>size</ic> ?
 
 - Que se passe-t-il si je change les valeurs dans <ic>vib</ic>, <ic>delay</ic>, <ic>room</ic> ou <ic>size</ic> ?
+
+<br>
 
 **Exercices :**
 
@@ -404,17 +426,23 @@ rhythm(0.5, 2, 8)::sound('sd').out()
 
 Nous allons créer un nouvel instrument à partir d'un son de base. Voici un premier son :
 
-${e("Notre son de base","beat(2)::sound('sine').note(50).ad(0, .5).out()",!0)};
+${e("Notre son de base","beat(2)::sound('sine').note(50).ad(0, .5).out()",!0)}
+
+<br>
 
 Ce son est assez ennuyeux. Nous allons ajouter quelques paramètres :
 
-${e("Beaucoup mieux !","beat(2)::sound('sine').note(50).fmi(2).fmh(2).ad(0, .5).out()",!0)};
+${e("Beaucoup mieux !","beat(2)::sound('sine').note(50).fmi(2).fmh(2).ad(0, .5).out()",!0)}
+
+<br>
 
 Nous allons aussi ajouter quelques effets intéressants :
 
 ${e("Ajout d'un écho",`beat(2)::sound('sine').note(50)
   .fmi(2).fmh(2).ad(1/16, 1.5)
   .delay(0.5).delayt(0.75).out()`,!0)}
+
+<br>
 
 Nous pouvons utiliser plusieurs techniques pour rendre le son plus dynamique :
 - générer des valeurs aléatoires pour les paramètres
@@ -427,6 +455,7 @@ beat(2)::sound('sine').note([50,55,57,62,66, 69, 74].mouseX())
   .ad(1/16, 1.5).delay(0.5).delayt(0.75)
   .out()`,!0)}
 
+<br>
 
 Un exemple final, le plus complexe jusqu'à présent :
 
@@ -436,6 +465,30 @@ beat(2)::sound('triangle')
   .fmi(usine(1/4)).fmh([1,2,0.5].beat())
   .ad(1/16, 1.5).delay(0.5).delayt(0.75)
   .room(0.5).size(8).lpf(usine(1/3)*4000)`,!0)}
+
+## Compléments
+
+${e("Quelques échantillons",`
+ab ade ades2 ades3 ades4 alex alphabet amencutup armora arp arpy auto
+baa baa2 bass bass0 bass1 bass2 bass3 bassdm bassfoo battles bd bend
+bev bin birds birds3 bleep blip blue bottle breaks125 breaks152
+breaks157 breaks165 breath bubble can casio cb cc chin circus clak
+click clubkick co coins control cosmicg cp cr crow d db diphone
+diphone2 dist dork2 dorkbot dr dr2 dr55 dr_few drum drumtraks e east
+electro1 em2 erk f feel feelfx fest fire flick fm foo future gab
+gabba gabbaloud gabbalouder glasstap glitch glitch2 gretsch gtr h
+hand hardcore hardkick haw hc hh hh27 hit hmm ho hoover house ht if
+ifdrums incoming industrial insect invaders jazz jungbass jungle juno
+jvbass kicklinn koy kurt latibro led less lighter linnhats lt made
+made2 mash mash2 metal miniyeah monsterb moog mouth mp3 msg mt mute
+newnotes noise noise2 notes numbers oc off outdoor pad padlong pebbles
+perc peri pluck popkick print proc procshort psr rave rave2 ravemono
+realclaps reverbkick rm rs sax sd seawolf sequential sf sheffield
+short sid sine sitar sn space speakspell speech speechless speedupdown
+stab stomp subroc3d sugar sundance tabla tabla2 tablex tacscan tech
+techno tink tok toys trump ul ulgab uxay v voodoo wind wobble world
+xmas yeah`,!0)}
+
 
 `},UB=r=>{const e=St(r);return`# Loading custom samples
 
